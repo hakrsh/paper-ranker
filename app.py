06d1453d31aa3f621dc757aba80bb6c54d18a227
hari <harikn77@gmail.com>
2022-10-21 00:40:51 +0530

    Change to public ip

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def search():
                            count = len(posts)
                            )
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
