e3cc01ed19feacff0f65eaa4874a973be538f282
hari <harikn77@gmail.com>
2022-11-11 22:34:51 +0530

    Change urllib to requests

--- api.py
+++ api.py
@@ -1,6 +1,5 @@
 import logging
-import urllib.request
-import json
+import requests
 from rank_mapper import get_rank
 ranks = {'A*', 'A', 'B', 'C', 'NA'}
 
@@ -24,48 +23,45 @@ def fetch_dblp(topic, hit_count=100):
         "h": hit_count,
         "format": "json"
     }
-    query_string = urllib.parse.urlencode(params)
-    url = url + "?" + query_string
+    res = requests.get(url, params=params)
     paper_list = list()
 
-    
-    with urllib.request.urlopen(url) as url_:
-        try:
-            data = json.loads(url_.read().decode())
-        except:
-            logging.info('Error in fetching data from DBLP')
-            return paper_list
-        # get relevant data
-        # check if key 'hit' exists in data['result']['hits']
-        if 'hits' not in data['result']:
-            return paper_list
-        if 'hit' not in data['result']['hits']:
-            return paper_list
-        for entry in data['result']['hits']['hit']:
-            paper_info = {}
-            if 'info' not in entry or 'type' not in entry['info']:
-                continue
+    try:
+        data = res.json()
+    except:
+        logging.info('Error in fetching data from DBLP')
+        return paper_list
+    # get relevant data
+    # check if key 'hit' exists in data['result']['hits']
+    if 'hits' not in data['result']:
+        return paper_list
+    if 'hit' not in data['result']['hits']:
+        return paper_list
+    for entry in data['result']['hits']['hit']:
+        paper_info = {}
+        if 'info' not in entry or 'type' not in entry['info']:
+            continue
 
-            if entry['info']['type'] == 'Conference and Workshop Papers':
-                if validate_json(entry['info']):
-                    paper_info['title'] = entry['info']['title']
-                    author_lst = list()
-                    auths = entry['info']['authors']['author']
-                    if isinstance(auths, dict):
-                        author_lst.append(auths['text'])
-                    else:
-                        for a in auths:
-                            author_lst.append(a['text'])
-                    paper_info['authors'] = author_lst
-                    paper_info['venue'] = entry['info']['venue'].split()[0].lower()
-                    paper_info['year'] = entry['info']['year']
-                    paper_info['url'] = entry['info']['url']
-                    rank = get_rank(paper_info['venue'])
-                    if rank not in ranks:
-                        logging.info('Rank not found for venue: ' + paper_info['venue'])
-                        rank = 'NA'
-                    paper_info['rank'] = rank
-                    paper_list.append(paper_info)
+        if entry['info']['type'] == 'Conference and Workshop Papers':
+            if validate_json(entry['info']):
+                paper_info['title'] = entry['info']['title']
+                author_lst = list()
+                auths = entry['info']['authors']['author']
+                if isinstance(auths, dict):
+                    author_lst.append(auths['text'])
+                else:
+                    for a in auths:
+                        author_lst.append(a['text'])
+                paper_info['authors'] = author_lst
+                paper_info['venue'] = entry['info']['venue'].split()[0].lower()
+                paper_info['year'] = entry['info']['year']
+                paper_info['url'] = entry['info']['url']
+                rank = get_rank(paper_info['venue'])
+                if rank not in ranks:
+                    logging.info('Rank not found for venue: ' + paper_info['venue'])
+                    rank = 'NA'
+                paper_info['rank'] = rank
+                paper_list.append(paper_info)
     return paper_list
 
 
@@ -77,35 +73,32 @@ def fetch_semantic_scholar(topic,h):
         "limit": h,
         "fields": "title,authors,venue,year,url" 
     }     
-    query_string = urllib.parse.urlencode( params ) 
-    url = url + "?" + query_string 
+    res = requests.get(url, params=params)
     paper_list = list()
 
-    
-    with urllib.request.urlopen(url) as url_:
-        try:
-            data = json.loads(url_.read().decode())
-        except:
-            logging.info('Error in fetching data from Semantic Scholar')
-            return paper_list
-        for entry in data['data']:
-            paper_info = {}
-            if validate_json(entry):
-                paper_info['title'] = entry['title']
-                author_lst = list()
-                for a in entry['authors']:
-                    if 'name' in a:
-                        author_lst.append(a['name'])
-                if len(author_lst) == 0:
-                    continue
-                paper_info['authors'] = author_lst
-                paper_info['venue'] = entry['venue']
-                paper_info['year'] = entry['year']
-                paper_info['url'] = entry['url']
-                rank = get_rank(paper_info['venue'])
-                if rank not in ranks:
-                    logging.info('Rank not found for venue: ' + paper_info['venue'])
-                    rank = 'NA'
-                paper_info['rank'] = rank
-                paper_list.append(paper_info)
+    try:
+        data = res.json()
+    except:
+        logging.info('Error in fetching data from Semantic Scholar')
+        return paper_list
+    for entry in data['data']:
+        paper_info = {}
+        if validate_json(entry):
+            paper_info['title'] = entry['title']
+            author_lst = list()
+            for a in entry['authors']:
+                if 'name' in a:
+                    author_lst.append(a['name'])
+            if len(author_lst) == 0:
+                continue
+            paper_info['authors'] = author_lst
+            paper_info['venue'] = entry['venue']
+            paper_info['year'] = entry['year']
+            paper_info['url'] = entry['url']
+            rank = get_rank(paper_info['venue'])
+            if rank not in ranks:
+                logging.info('Rank not found for venue: ' + paper_info['venue'])
+                rank = 'NA'
+            paper_info['rank'] = rank
+            paper_list.append(paper_info)
     return paper_list
